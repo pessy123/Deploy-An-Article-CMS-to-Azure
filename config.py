ee34ceaef81de2adf326c70526fd84442a377817
anyulled <anyulled@gmail.com>
2021-05-09 23:57:21 +0200

    feat: configure access
    - blob account
    - sql server

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'anyulstorage2021'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'HsmcN51v91quYY988zWy985YggboYLVdRxbzrElg9VxycCgpNWKeUfiZmb73CnOvmEDqcfJh3oo++LsvEmzIYw=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'project-one.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'project-one'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacity'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Temporal1'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
