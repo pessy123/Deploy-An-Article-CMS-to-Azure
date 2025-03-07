ab17723bfd4fdf8aba362ff7b3df7d492598ad30
Prasanna Muppidi <31557847+pessy123@users.noreply.github.com>
2025-03-07 16:24:09 -0600

    Update forms.py

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -1,8 +1,9 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
 
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+from wtforms.widgets import TextArea
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -10,10 +11,11 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    sub_title = StringField('Sub Title')
     author = StringField('Author', validators=[DataRequired()])
     body = TextAreaField('Body', validators=[DataRequired()])
     image_path = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    remove_image_path = BooleanField('Remove Image')
     submit = SubmitField('Save')
